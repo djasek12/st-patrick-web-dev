10a2be6fe8e49c1acc8927b05c52641d4ba2b7ed
Daniel Jasek <djasek1255@gmail.com>
2018-11-05 22:23:05 -0500

    template can now read from a variety of JSON files

---
+++ csv-to-json.py
@@ -0,0 +1,12 @@
+import csv, json
+fileName = 'C:\\Users\\dj125\\Downloads\\st-patrick-ministry-responses - Sheet1.tsv'
+
+with open(fileName, encoding="utf-8") as f:
+    reader = csv.DictReader(f, delimiter="\t")
+    data = list(reader)
+
+json_data = json.dumps(data)
+
+with open('data.json', 'w') as outfile:
+    outfile.write("var data=")
+    json.dump(json_data, outfile)
