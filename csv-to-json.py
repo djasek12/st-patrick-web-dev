1f059f56cdfd6e6bb179759c299794ad22b4d2d6
Daniel Jasek <djasek1255@gmail.com>
2018-12-02 16:44:15 -0500

    more email obfuscation

--- csv-to-json.py
+++ csv-to-json.py
@@ -1,4 +1,4 @@
-import csv, json
+import csv, json, re
 fileName = 'C:\\Users\\dj125\\Downloads\\st-patrick-ministry-responses - Sheet1.tsv'
 
 with open(fileName, encoding="utf-8") as f:
@@ -6,6 +6,11 @@ with open(fileName, encoding="utf-8") as f:
     data = list(reader)
 
 json_data = json.dumps(data)
+print(json_data)
+
+emailRegex = r'([\w\.-]+)@([\w\.-]+)\.([\w\.-]+)'
+encodedEmailRegex = r'\1%\2!\3'
+json_data = re.sub(emailRegex, encodedEmailRegex, json_data)
 
 with open('organizationInfo.js', 'w') as outfile:
     outfile.write("var data=")
